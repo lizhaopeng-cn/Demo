660088f3cc4027fcabc2f707798036265517670b
lzp <lizhaopeng.cn@gmail.com>
2017-06-04 20:29:05 +0800

    recycleview

--- RecycleView/src/main/java/com/example/recycleview/MainActivity.java
+++ RecycleView/src/main/java/com/example/recycleview/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.recycleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private MyRecyclerViewAdapter adapter;
+    private List<String> datas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datas = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            datas.add("数据"+i);
+        }
+        
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        //设置布局
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new MyRecyclerViewAdapter(this,datas);
+        mRecyclerView.setAdapter(adapter);
     }
 }
