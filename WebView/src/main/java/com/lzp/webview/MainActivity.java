6f966d1329b6747711ba1ddd4332ab83efd15858
lzp <lizhaopeng.cn@gmail.com>
2017-07-12 19:37:58 +0800

    WebView

---
+++ WebView/src/main/java/com/lzp/webview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lzp.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("http://www.baidu.com");
+    }
+}
