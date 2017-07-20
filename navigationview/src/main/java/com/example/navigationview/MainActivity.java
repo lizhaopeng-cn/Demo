528a1f63fb17380a5136f7b6eac6383ed91b36a6
lzp <lizhaopeng.cn@gmail.com>
2017-07-21 00:41:30 +0800

    NavigationView

--- navigationview/src/main/java/com/example/navigationview/MainActivity.java
+++ navigationview/src/main/java/com/example/navigationview/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.navigationview;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+        initData();
+        setOnClick();
+    }
+
+    private void findView() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+    }
+
+    private void initData() {
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("Title");
+        toolbar.setSubtitle("Subtitle");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        toolbar.setSubtitleTextColor(getResources().getColor(R.color.white));
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open,R.string.close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    private void setOnClick() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(fab,"Toast",Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.nav_camera){
+                    Snackbar.make(fab,"nav_camera",Snackbar.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_gallery) {
+                    Snackbar.make(fab,"nav_gallery",Snackbar.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_slideshow) {
+                    Snackbar.make(fab,"nav_slideshow",Snackbar.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_manage) {
+                    Snackbar.make(fab,"nav_manage",Snackbar.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_share) {
+                    Snackbar.make(fab,"nav_share",Snackbar.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_send) {
+                    Snackbar.make(fab,"nav_send",Snackbar.LENGTH_SHORT).show();
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            Snackbar.make(fab,"action_settings",Snackbar.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
