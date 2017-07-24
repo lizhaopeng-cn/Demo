850dec88cd922a6643909e7f174d320e6c5f0a32
lzp <lizhaopeng.cn@gmail.com>
2017-07-24 21:22:30 +0800

    Rxjava

---
+++ RxJava/src/main/java/com/lzp/rxjava/Course.java
@@ -0,0 +1,21 @@
+package com.lzp.rxjava;
+
+/**
+ * Created by lzp on 2017/7/22.
+ */
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
