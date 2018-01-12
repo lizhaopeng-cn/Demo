7b599aeeb0ed8f5eb0b726f3aec775d7aa5bd1d5
lzp <lizhaopeng.cn@gmail.com>
2018-01-12 11:34:56 +0800

    增加悬浮球动画操作

---
+++ CoordinatorLayout/src/main/java/com/lzp/coordinatorlayout/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.lzp.coordinatorlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lzp48947 on 2018/1/11.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
